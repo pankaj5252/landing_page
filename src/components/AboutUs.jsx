6c120471813aadae483df2f7f05e6d50fb58cc65
Pankaj Kale <pbkale25@gmail.com>
2025-03-25 16:49:54 +0530

    Initial commit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -62,13 +62,13 @@ const AboutUs = () => {
               </div>
             </div>
             <div className="col-md-6 p-3">
-              <div>
+              <div style={{ width: "100%", maxWidth: "800px", margin: "auto" }}>
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242117.70906969727!2d73.69815309340439!3d18.524870610788835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1710937390606!5m2!1sen!2sin"
-                  width="600"
+                  width="100%"
                   height="450"
                   allowFullScreen
-                  style={{ border: 'none' }}
+                  style={{ border: "none" }}
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
